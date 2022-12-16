ac622448271cc2b80fb1c9c206e89eb8ccadd8b8
Jhonny Sarmiento <jhonny.sarmiento@o365.unab.edu.co>
2022-12-15 23:17:16 -0500

    actualizacion transaccionDao

--- backend/banca/src/main/java/com/unab/banca/Dao/TransaccionDao.java
+++ backend/banca/src/main/java/com/unab/banca/Dao/TransaccionDao.java
@@ -1,5 +1,7 @@
 package com.unab.banca.Dao;
-
-public class TransaccionDao {
-    
+import com.unab.banca.Models.Transaccion;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface TransaccionDao extends CrudRepository< Transaccion, String>  {
 }
